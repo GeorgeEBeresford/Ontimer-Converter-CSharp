8206454fd815d0ac7a047502b5ffa929efdc751d
George Beresford <george.e.beresford@gmail.com>
2022-12-17 21:04:05 +0000

    Script also adds ontimer to regular commands

--- OntimerConverter.UnitTest/ScriptConverterTest.cs
+++ OntimerConverter.UnitTest/ScriptConverterTest.cs
@@ -11,24 +11,45 @@ namespace OntimerConverter.UnitTest
         }
 
         [TestMethod]
-        public void ConvertScripts()
+        public void AddOntimerToCommands()
         {
-            // Already ontimered script with semi colon delimiters
+            // Semi colon delimiters
+            string nonOntimerScript = @"echo cool;echo cool2;echo cool3;echo cool4";
+            string result = ScriptConverter.ConvertScript(nonOntimerScript);
+
+            Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
+
+            // Newline delimiters
+            nonOntimerScript = "echo cool\necho cool2\necho cool3\necho cool4";
+            result = ScriptConverter.ConvertScript(nonOntimerScript);
+
+            Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
+
+            // Semicolon and newline delimiters
+            nonOntimerScript = "echo cool;\necho cool2;\necho cool3;\necho cool4";
+            result = ScriptConverter.ConvertScript(nonOntimerScript);
+
+            Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
+        }
+
+        [TestMethod]
+        public void ChangeOntimerDelay()
+        {
+            // Semi colon delimiters
             string fullyOntimerScript = @"ontimer 0 echo cool;ontimer 30 echo cool2; ontimer 60 echo cool3; ontimer 90 echo cool4";
             string result = ScriptConverter.ConvertScript(fullyOntimerScript);
 
             Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
 
-            // Already ontimered script with newline delimiters
+            // Newline delimiters
             fullyOntimerScript = "ontimer 0 echo cool\nontimer 30 echo cool2\n ontimer 60 echo cool3\nontimer 90 echo cool4";
             result = ScriptConverter.ConvertScript(fullyOntimerScript);
 
             Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
 
-            // Already ontimered script with semicolon and newline delimiters
+            // Semicolon and newline delimiters
             fullyOntimerScript = "ontimer 0 echo cool;\nontimer 30 echo cool2;\n ontimer 60 echo cool3;\nontimer 90 echo cool4;";
             result = ScriptConverter.ConvertScript(fullyOntimerScript);
-
             Assert.AreEqual("ontimer 0 \"echo cool\"\nontimer 10 \"echo cool2\"\nontimer 20 \"echo cool3\"\nontimer 30 \"echo cool4\"", result);
         }
     }
